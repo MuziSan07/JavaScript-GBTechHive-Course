c331ecc55d55e4aab5e432c8d96bc2e674266f0f
Ali Basit. <shumailhaider678@gmail.com>
2025-12-23 22:42:19 -0800

    Add files via upload

---
+++ ClassOne.js
@@ -0,0 +1,31 @@
+console.log("Hello World")
+
+// This is our JS Comment
+
+
+const UserName = "Ali"
+let UserEmail = "admin@gmail.com"
+var UserPass = "50269"
+Location = "Gilgit"
+
+let PhoneNumber;
+
+// UserName = "Basit"
+
+console.log(UserName) 
+
+UserEmail = "newadmin@gmail.com"
+UserPass  = "112233"
+Location = "Diamer"
+
+/*
+What is Scope?
+
+Why we should use var!
+
+*/
+
+
+console.table([UserEmail, UserPass, Location])
+
+console.log(PhoneNumber);
